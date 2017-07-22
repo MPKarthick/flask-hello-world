91b7d83ebec4e6fe0e0f2a41b6eaf3956468ddd0
Karthick MP <sigmasix06@gmail.com>
2017-07-22 22:22:39 +0530

    added dynamic URL

--- app.py
+++ app.py
@@ -17,6 +17,22 @@ def hello_world():
 def hello_world1():
     return "Hello World1"
 
+# dynamic route where tha URL is converted as string
+@app.route("/<search_query>")
+def search(search_query):
+    return search_query
+
+# dynamic route where u want to pass or convert to string
+# this is done by using convertors
+# int --> integer
+# float --> accepts decimal value
+# path --> same as string accepts slashes
+# any --> matches 1 of the item provided
+# uuid --> accepts UUID string
+@app.route("/<int:post_id>")
+def search_number(post_id):
+    return "post id: %d"%post_id
+
 # start the development server using run() method
 if __name__ == '__main__':
     app.run()
